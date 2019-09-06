b8c7f96a52ce6c18a217bf1bbd86e7c471867c8d
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-09-06 19:26:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -4,5 +4,4 @@
 
 ### Summary
 
-- Algorithms practice in the frequency_pattern files.
-- Expanding RPG Game from the previous day. More characters, special abilities, usage of items, etc.
+The `frequency_pattern` files are some algorithms practice. The `rpg-v2.py` is yesterdays RPG that is expanded to include more characters that the user can play, special abilities, usage of items, etc.
--- rpg-v2.py
+++ rpg-v2.py
@@ -6,6 +6,8 @@ Exercise is to expand on the previous days RPG game. More characters, adding Ite
 """
 import random
 
+# Parent class of both User Characters and Enemies
+
 
 class Character(object):
 
@@ -15,13 +17,22 @@ class Character(object):
         self.power = power
 
     def attack(self, enemy):
-
-        enemy.health -= self.power
+        medic_chance = random.random()
+        # if the class you are attacking is a Medic
+        if enemy.__class__.__name__ == 'Medic':
+            if medic_chance <= 0.20:
+                enemy.health -= self.power
+                enemy.health += 2
+                print("Your Medic special ability kicked in. You gained 2 health.")
+        else:
+            enemy.health -= self.power
 
     def alive(self):
 
         if self.health > 0:
             return True
+
+    # Todo will need to expand this for other classes that are created
 
     def print_status(self):
 
@@ -30,6 +41,8 @@ class Character(object):
         elif self.__class__.__name__ == 'Zombie':
             print(
                 f"The Zombie has {self.health} health and {self.power} power.")
+
+# ! Below are the classes the user can choose to play as: Hero, Medic, Shadow
 
 
 class Hero(Character):
@@ -48,26 +61,66 @@ class Hero(Character):
                 f"You do {self.power} damage to the {enemy.__class__.__name__}.")
 
 
+class Medic(Character):
+    pass
+
+# ! Below are the classes the user fights: Goblin, Zombie
+
+
 class Zombie(Character):
     def never_die(self, enemy):
         if self.health <= 6:
             self.health = 6
 
 
+# Todo
 # Character Stats
 hero_health = 10
 hero_power = 5
+
+medic_health = 10
+medic_power = 3
+
+# TODO Create all the character objects
+# Character Objects
+our_hero = Hero(hero_health, hero_power)
+medic = Medic(medic_health, medic_power)
+
+# Enemy Stats
 zombie_health = 6
 zombie_power = 3
 
-# Character Objects
-our_hero = Hero(hero_health, hero_power)
+# TODO Create all enemy objects
+# Enemy Objects
 zombie = Zombie(zombie_health, zombie_power)
+
+# Dictionary of Initialized Characters
+character_dict = {
+    1: our_hero,
+    2: medic
+}
+
+# TODO We can randomly select an enemy for our hero to face!
+# TODO Store all of the Enemy Objects in an empty array.
+enemy_list = []
+enemy_list.append(zombie)
 
 
 def main():
-    while zombie.alive() and our_hero.alive():
-        our_hero.print_status()
+    # ! Make sure this text keeps up to date with how many User Character options there are
+    print(f"""
+    Welcome to Nep's RPG Game. Please choose to play as either a Hero or Medic.
+    A Hero has {our_hero.health} health and {our_hero.power} power.
+    A Medic has {medic.health} health and {medic.power} power. 
+    Medic's can some times regenerate 2 health when they're attacked.
+    """)
+
+    character_choice = int(input("Type 1 for Hero. Type 2 for Medic >> "))
+
+    user_character = character_dict[character_choice]
+    print()
+    while zombie.alive() and user_character.alive():
+        user_character.print_status()
         zombie.print_status()
 
         print()
